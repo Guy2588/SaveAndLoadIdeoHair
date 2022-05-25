998de948ec678cbdf110e729ed168d073d7175ca
Guy2588 <101416040+Guy2588@users.noreply.github.com>
2022-05-25 17:28:34 -0400

    Add files via upload

---
+++ SaveAndLoadIdeoHair/Dialog_IdeoHairList_Save.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace SaveAndLoadIdeoHair
+{
+    class Dialog_IdeoHairList_Save : Dialog_IdeoHairList
+    {
+        private Ideo savingIdeo; //pass in a whole ideoligion, but we don't have to save the entire thing, just pieces that we need
+        //potential problem when loading and have to repackage the pieces into an ideoligion?
+
+        protected override bool ShouldDoTypeInField => true; //no idea what this does, maybe allows saving while a game is in progress? I'll have to follow it to find out...
+
+        //constructor, called in the patched method of Dialog_EditIdeoStyleItems
+        public Dialog_IdeoHairList_Save(Ideo ideo) { 
+            interactButLabel = "OverwriteButton".Translate();
+            typingName = ideo.name;
+            savingIdeo = ideo;
+        }
+
+        protected override void DoFileInteraction(string fileName)
+        {
+            //original code delegates this to a helper class
+            throw new NotImplementedException();
+            Messages.Message("SavedAs".Translate(fileName), MessageTypeDefOf.SilentInput, historical: false); //can I ignore the warning?
+            Log.Error("Not implemented");
+        }
+    }
+}
